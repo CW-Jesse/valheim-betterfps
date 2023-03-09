14f17483329084fdc1ea2e4f4afd44fe5955b3df
CW Jesse <jesse@confusedwizard.com>
2023-03-08 23:25:29 -0800

    ZNetScene updates less often

---
+++ CW_Jesse.BetterFPS/BetterFps_Patch_ZNet.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using HarmonyLib;
+using UnityEngine;
+
+namespace CWJesse.BetterFPS {
+    
+    [HarmonyPatch]
+    public class BetterFps_Patch_ZNet {
+        private const float MIN_UPDATE_DELTA_TIME = 0.1f;
+        
+        private static Dictionary<int, float> ZNetSceneLastUpdateTime = new Dictionary<int, float>();
+        
+        [HarmonyPatch(typeof(ZNetScene), "Update")]
+        [HarmonyPrefix]
+        public static bool ZNetSceneUpdates(ref ZNetScene __instance) {
+
+            int instanceId = __instance.GetHashCode();
+            if (!ZNetSceneLastUpdateTime.TryGetValue(instanceId, out float lastUpdate)) { ZNetSceneLastUpdateTime[instanceId] = Time.time; }
+            
+            if (Time.time - lastUpdate < MIN_UPDATE_DELTA_TIME) return false;
+            ZNetSceneLastUpdateTime[instanceId] = Time.time;
+            return true;
+        }
+    }
+}
