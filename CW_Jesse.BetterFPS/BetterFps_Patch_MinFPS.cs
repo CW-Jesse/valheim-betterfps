cce39835c6f3133f7a11ddf3a55472232862a8cd
CW Jesse <jesse@confusedwizard.com>
2023-03-08 18:09:14 -0800

    increased accuracy of in-game FPS display renamed ReducePhysicsCalculations -> ContinuePhysicsNextFrameIfNeeded to not mislead people

--- CW_Jesse.BetterFPS/BetterFps_Patch_MinFPS.cs
+++ CW_Jesse.BetterFPS/BetterFps_Patch_MinFPS.cs
@@ -1,5 +1,7 @@
+using System.Net.Mime;
 using HarmonyLib;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace CWJesse.BetterFPS {
     [HarmonyPatch]
@@ -7,9 +9,29 @@ namespace CWJesse.BetterFPS {
 
         [HarmonyPatch(typeof(Game), "Awake")]
         [HarmonyPostfix]
-        public static void ReducePhysicsCalculations() {
+        public static void ContinuePhysicsNextFrameIfNeeded() {
             Time.maximumDeltaTime = 1.0f/30.0f;
         }
-        
+
+        [HarmonyPatch(typeof(ConnectPanel), "UpdateFps")]
+        [HarmonyPrefix]
+        public static bool MakeFpsCounterAccurate(
+            ref float ___m_frameTimer,
+            ref int ___m_frameSamples,
+            ref Text ___m_fps,
+            ref Text ___m_frameTime) {
+            
+            ___m_frameTimer += Time.unscaledDeltaTime;
+            ++___m_frameSamples;
+            if ((double) ___m_frameTimer <= 1.0) return false;
+            
+            float num = ___m_frameTimer / (float) ___m_frameSamples;
+            ___m_fps.text = (1f / num).ToString("0.0");
+            ___m_frameTime.text = "( " + (num * 1000f).ToString("00.0") + "ms )";
+            ___m_frameSamples = 0;
+            ___m_frameTimer = 0.0f;
+            return false;
+        }
+
     }
 }
