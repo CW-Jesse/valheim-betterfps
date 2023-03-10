a967a62095121c7728ffd280be7243323dd83cb3
CW Jesse <jesse@confusedwizard.com>
2023-03-10 15:52:05 -0800

    frameTimeAverage changes faster when FPS is lower Time.maximumDeltaTime changes to discourage frame drops greater than 25% of current FPS Time.maximumDeltaTime min is now 5 FPS and can go as high as 20 FPS refactoring of BetterFps_Patch_MinFPS

--- CW_Jesse.BetterFPS/BetterFps_Patch_MinFPS.cs
+++ CW_Jesse.BetterFPS/BetterFps_Patch_MinFPS.cs
@@ -1,10 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Reflection;
-using System.Reflection.Emit;
-using System.Security.AccessControl;
 using BepInEx.Configuration;
 using HarmonyLib;
 using UnityEngine;
@@ -14,8 +7,6 @@ namespace CWJesse.BetterFPS {
     [HarmonyPatch]
     public class BetterFps_Patch_MinFPS {
         private static ConfigEntry<int> ConfigMinFps;
-        
-        private const float TIME_MEASUREMENT_VELOCITY = 1.0f/16.0f;
 
         private const float MIN_FIXED_DELTA_TIME = 1.0f / 50.0f;
         private const float MAX_FIXED_DELTA_TIME = 1.0f / 20.0f;
@@ -27,8 +18,8 @@ namespace CWJesse.BetterFPS {
         private const float MAX_FRAME_TIME = 0.2f;
         private const float MAX_HICCUP_PERCENT = 1.25f;
 
-        private static float frameTimeAverage = Time.unscaledDeltaTime;
-        // private static float desiredFps = 1.0f / 30.0f;
+        private static float FrameTimeAverage = Time.unscaledDeltaTime;
+        private static float MaxFrameTime = Time.fixedDeltaTime;
         
         public static void InitConfig(ConfigFile config) {
             ConfigMinFps = config.Bind(
@@ -37,33 +28,18 @@ namespace CWJesse.BetterFPS {
                 30,
                 new ConfigDescription("Automatically reduces frequency of physics & animation calculations to meet this target FPS.", 
                     new AcceptableValueRange<int>(MIN_MINFPS, MAX_MINFPS)));
-
-            // ConfigMinFps.SettingChanged += (sender, args) => desiredFps = ConfigMinFps.Value;
         }
-        
-        // [HarmonyPatch(typeof(Game), "Awake")]
-        // [HarmonyPostfix]
-        // public static void Start() {
-        //     Time.maximumDeltaTime = 1.0f / 20.0f; // this slows the game down, which feels worse than a low frame rate
-        // }
-        
-        private static float maxFrameTime = Time.fixedDeltaTime;
-        private static float prevMaximumDeltaTime = Time.maximumDeltaTime;
         
         [HarmonyPatch(typeof(Game), "Update")]
         [HarmonyPostfix]
         public static void MeetMinFps() {
             if (!BetterFps.ConfigEnabled.Value) return;
             
-            maxFrameTime = Mathf.Clamp(maxFrameTime * frameTimeAverage * ConfigMinFps.Value, MIN_FIXED_DELTA_TIME, MAX_FIXED_DELTA_TIME);
-            Time.fixedDeltaTime = maxFrameTime;
+            MaxFrameTime = Mathf.Clamp(MaxFrameTime * FrameTimeAverage * ConfigMinFps.Value, MIN_FIXED_DELTA_TIME, MAX_FIXED_DELTA_TIME);
+            Time.fixedDeltaTime = MaxFrameTime;
             
-            // prevent hiccups greater than 25% at low frame rates, but also try for a minimum of 5 FPS
-            var newMaximumDeltaTime = Mathf.Clamp(frameTimeAverage * MAX_HICCUP_PERCENT, MIN_FRAME_TIME, MAX_FRAME_TIME);
-            if (newMaximumDeltaTime != prevMaximumDeltaTime) {
-                Time.maximumDeltaTime = newMaximumDeltaTime;
-                prevMaximumDeltaTime = newMaximumDeltaTime;
-            }
+            // prevent hiccups greater than 25% at low frame rates, but also try for a minimum FPS of MIN_FRAME_TIME
+            Time.maximumDeltaTime = Mathf.Clamp(FrameTimeAverage * MAX_HICCUP_PERCENT, MIN_FRAME_TIME, MAX_FRAME_TIME);
         }
         
         [HarmonyPatch(typeof(ConnectPanel), "UpdateFps")]
@@ -71,25 +47,11 @@ namespace CWJesse.BetterFPS {
         public static bool FpsCounterImprovements(ref Text ___m_fps, ref Text ___m_frameTime) {
             if (!BetterFps.ConfigEnabled.Value) return true;
             
-            frameTimeAverage = Mathf.Lerp(frameTimeAverage, Time.unscaledDeltaTime, Time.unscaledDeltaTime); // the lower the frame rate, the faster it finds the current frame rate
-            ___m_fps.text = (1.0f/frameTimeAverage).ToString("0");
-            ___m_frameTime.text = $"({(frameTimeAverage * 1000f).ToString("00")}ms)";
+            FrameTimeAverage = Mathf.Lerp(FrameTimeAverage, Time.unscaledDeltaTime, Time.unscaledDeltaTime); // the lower the frame rate, the faster it finds the current frame rate
+            ___m_fps.text = $"{(1.0f/FrameTimeAverage):0})";
+            ___m_frameTime.text = $"({(FrameTimeAverage * 1000f):0}ms)";
 
             return false;
         }
-
-        // [HarmonyPatch(typeof(ConnectPanel), "UpdateFps")]
-        // [HarmonyTranspiler]
-        // public static IEnumerable<CodeInstruction> MakeFpsCounterAccurate(IEnumerable<CodeInstruction> instructions) {
-        //     MethodInfo deltaTimeFI = AccessTools.Property(typeof(Time), nameof(Time.deltaTime)).GetMethod;
-        //     MethodInfo unscaledDeltaTimeFI = AccessTools.Property(typeof(Time), nameof(Time.unscaledDeltaTime)).GetMethod;
-        //
-        //     foreach (var i in instructions) {
-        //         if (i.Calls(deltaTimeFI))
-        //             yield return new CodeInstruction(OpCodes.Call, unscaledDeltaTimeFI);
-        //         else
-        //             yield return i;                
-        //     }
-        // }
     }
 }
