2271d7b740cab8c8e9dd26e56e564fb9ad20b792
CW Jesse <jesse@confusedwizard.com>
2023-03-10 18:42:00 -0800

    fixed a bug where mod was only working properly when showing in-game FPS display removed ability to set target frame rate for now to prevent people from setting it too high

--- CW_Jesse.BetterFPS/BetterFps_Patch_MinFPS.cs
+++ CW_Jesse.BetterFPS/BetterFps_Patch_MinFPS.cs
@@ -11,7 +11,7 @@ namespace CWJesse.BetterFPS {
         private const float MIN_FIXED_DELTA_TIME = 1.0f / 50.0f;
         private const float MAX_FIXED_DELTA_TIME = 1.0f / 20.0f;
 
-        private const int DEFAULT_MINFPS = 30;
+        private const int DEFAULT_MINFPS = 20;
         private const int MIN_MINFPS = 5;
         private const int MAX_MINFPS = 120;
         
@@ -20,6 +20,8 @@ namespace CWJesse.BetterFPS {
         private const float MAX_HICCUP_PERCENT = 1.25f;
 
         private static float FrameTimeAverage = Time.unscaledDeltaTime;
+        // private static float FrameTimeAverageAverage = Time.unscaledDeltaTime;
+        // private static float FrameTimeAverageAcceleration = 0.0f;
         private static float MaxFrameTime = Time.fixedDeltaTime;
         
         public static void InitConfig(ConfigFile config) {
@@ -36,7 +38,11 @@ namespace CWJesse.BetterFPS {
         public static void MeetMinFps() {
             if (!BetterFps.ConfigEnabled.Value) return;
             
-            MaxFrameTime = Mathf.Clamp(MaxFrameTime * FrameTimeAverage * ConfigMinFps.Value, MIN_FIXED_DELTA_TIME, MAX_FIXED_DELTA_TIME);
+            FrameTimeAverage = Mathf.Lerp(FrameTimeAverage, Time.unscaledDeltaTime, Time.unscaledDeltaTime); // the lower the frame rate, the faster it finds the current frame rate
+            // FrameTimeAverageAverage = Mathf.Lerp(FrameTimeAverage, FrameTimeAverage, Time.unscaledDeltaTime);
+            // FrameTimeAverageAcceleration = FrameTimeAverage - FrameTimeAverageAverage;
+            
+            MaxFrameTime = Mathf.Clamp(MaxFrameTime * FrameTimeAverage * FrameTimeAverage + 3, MIN_FIXED_DELTA_TIME, MAX_FIXED_DELTA_TIME);
             Time.fixedDeltaTime = MaxFrameTime;
             
             // prevent hiccups greater than 25% at low frame rates, but also try for a minimum FPS of MIN_FRAME_TIME
@@ -48,7 +54,6 @@ namespace CWJesse.BetterFPS {
         public static bool FpsCounterImprovements(ref Text ___m_fps, ref Text ___m_frameTime) {
             if (!BetterFps.ConfigEnabled.Value) return true;
             
-            FrameTimeAverage = Mathf.Lerp(FrameTimeAverage, Time.unscaledDeltaTime, Time.unscaledDeltaTime); // the lower the frame rate, the faster it finds the current frame rate
             ___m_fps.text = $"{(1.0f/FrameTimeAverage):0}";
             ___m_frameTime.text = $"({(FrameTimeAverage * 1000f):0}ms)";
 
